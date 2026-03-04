2867e29b6d857159a798ca5885157a84ce9aea3b
lonesomeGoblin <lonesomegoblin@outlook.com>
2026-03-04 20:58:08 +0800

    commit: 完成 prisma 基本配置

---
+++ packages/service/client.js
@@ -0,0 +1,5 @@
+import { PrismaClient } from "./prisma/client/client.ts";
+
+const prisma = new PrismaClient();
+
+export default prisma;
--- packages/service/index.js
+++ packages/service/index.js
@@ -1,7 +1,28 @@
-import express from 'express';
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import prisma from "./client.js";
 
 const app = express();
 
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/add", async (req, res) => {
+  const data = await prisma.user.create({
+    data: {
+      nickname: `用户${new Array(8).fill("").map(() => Math.floor(Math.random() * 10))}`,
+      phoneNumber: "166****8888",
+      balance: 100,
+      email: "******@gmail.com",
+    },
+  });
+});
+
 app.listen(3000, () => {
-    console.log(`Server started on port 3000!`);
-})
+  console.log(`Server started on port 3000!`);
+});
